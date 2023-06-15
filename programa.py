fdedc4958e250797de27b971cc5a27ea3b67111c
Jhonathan Ordóñez <135990992+Terratenienteandres@users.noreply.github.com>
2023-06-15 09:46:25 -0500

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,7 @@
+from matematica import sumar, restar, multiplicar, dividir
+from algebra import distancia
+print(sumar(2, 3))
+print(restar(4, 2))
+print(multiplicar(1, 2))
+print(dividir(2 , 3))
+print(distancia(1, 2, 3, 4))
